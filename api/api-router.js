1785a197f9da76f9a97f53d1a84e9352041b4ed4
B Severino <biancasev@gmail.com>
2020-01-28 15:51:22 -0500

    Created logout endpoint and set up restricted router

--- api/api-router.js
+++ api/api-router.js
@@ -2,8 +2,10 @@ const router = require('express').Router()
 
 const authRouter = require('./auth/auth-router.js')
 const userRouter = require('./users/user-router.js')
+const restrictedRouter = require('./restricted/restricted-router.js')
 
 router.use('/auth', authRouter)
 router.use('/users', userRouter)
+router.use('/restricted', restrictedRouter)
 
 module.exports = router
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -38,4 +38,19 @@ router.post('/login', (req, res) => {
         })
 })
 
+router.get('/logout', (req, res) => {
+    if (req.session) {
+        req.session.destroy(err => {
+            if (err) {
+                console.log(err)
+                res.status(500).json({ message: 'Error logging out.' })
+            } else {
+                res.status(204).end()
+            }
+        })
+    } else {
+        res.status(204).end()
+    }
+})
+
 module.exports = router
